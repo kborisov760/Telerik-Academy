c5bb0319e3d3570cec99ee5296452dd9ffc5ae78
Kiril Borisov <borisov.kiril760@gmail.com>
2015-06-23 11:59:26 +0300

    Homework submission
    Code formatting homework added

---
+++ 08. High-Quality-Code/01. Formatting-Homework/01.Events.cs/Events/MultiDictionary.cs
@@ -0,0 +1,28 @@
+﻿namespace Events
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MultiDictionary<T1, T2>
+    {
+        private bool p;
+
+        public MultiDictionary(bool p)
+        {
+            // TODO: Complete member initialization
+            this.p = p;
+        }
+
+        internal void Add(string p, Event newEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Remove(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 08. High-Quality-Code/01. Formatting-Homework/01.Events.cs/Events/OrderedBag.cs
@@ -0,0 +1,22 @@
+﻿namespace Events
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class OrderedBag<T>
+    {
+        internal void Add(Event newEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object RangeFrom(Event p1, bool p2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object VieweventsToShow { get; set; }
+    }
+}

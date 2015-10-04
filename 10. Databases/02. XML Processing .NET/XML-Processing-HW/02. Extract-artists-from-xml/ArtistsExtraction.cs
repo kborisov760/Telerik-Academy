74e92e9183fa5c33409e4189524881e2ded9ae63
Kiril Borisov <borisov.kiril760@gmail.com>
2015-10-05 01:37:31 +0300

    Added functionality for reading and parsing XML, logic for counting artist's albums should be added

--- 10. Databases/02. XML Processing .NET/XML-Processing-HW/02. Extract-artists-from-xml/ArtistsExtraction.cs
+++ 10. Databases/02. XML Processing .NET/XML-Processing-HW/02. Extract-artists-from-xml/ArtistsExtraction.cs
@@ -1,11 +1,34 @@
 ﻿namespace ExtractArtistsFromXML
 {
     using System;
+    using System.Collections.Generic;
+    using System.Xml;
 
     class ArtistsExtraction
     {
         static void Main()
         {
+            XmlDocument calatogDocument = new XmlDocument();
+            calatogDocument.Load("../../../catalog.xml");
+            XmlNode mainNode = calatogDocument.DocumentElement;
+            var allArtists = new Dictionary<string, int>();
+            foreach (XmlNode node in mainNode.ChildNodes)
+            {
+                //allArtists.Add(node["artist"].InnerText, node[]);
+                
+
+            }
+
+            Console.WriteLine("List of artists: {0}", String.Join(", ", allArtists));
+        }
+
+        //TODO: Implement logic for counting all times given artist's name is matching XML within specific attr
+
+        public int CountAuthorAlbums(XmlAttribute artist)
+        {
+            int artistCounter = 0;
+            
+            return 0;
         }
     }
 }

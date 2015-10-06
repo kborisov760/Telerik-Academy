68bd595d4753a37afdaf6748f8a28f2ee0180668
Kiril Borisov <borisov.kiril760@gmail.com>
2015-10-07 02:00:10 +0300

    Some files added

---
+++ 10. Databases/02. XML Processing .NET/XML-Processing-HW/03.ExtractArtistFromXml-XPath/ArtistExtractionXPath.cs
@@ -0,0 +1,79 @@
+﻿namespace XMLProcessing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml;
+    using System.Xml.XPath;
+
+    public class ArtistExtractionXPath
+    {
+        static void Main()
+        {
+            XmlDocument calatogDocument = new XmlDocument();
+            calatogDocument.Load("../../../catalog.xml");
+            string xPathQuery = "/catalog/album/artist";
+            XmlNodeList artistList = calatogDocument.SelectNodes(xPathQuery);
+
+            var allArtists = new HashSet<string>();
+            var artistHashSet = new HashSet<string>();
+
+            for (int i = 0; i < artistList.Count; i++)
+            {
+                allArtists.Add(artistList[i].InnerText);
+            }
+
+            Console.WriteLine("List of artists: {0}", String.Join(", ", allArtists));
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Number of albums for every artist: ");
+            
+              xPathQuery = "/catalog/album";
+
+            XmlNodeList wholeDocument = calatogDocument.SelectNodes(xPathQuery);
+
+            for (int i = 0; i < wholeDocument.Count; i++)
+            {
+                var currentNode = wholeDocument[i];
+                if (currentNode.Name == "artist" && !artistHashSet.Contains(currentNode.InnerText))
+                {
+                    artistHashSet.Add(currentNode.InnerText);
+                    Console.WriteLine("{0} has {1} albums(s).", currentNode.InnerText, CountAuthorAlbums(currentNode.InnerText));
+                }
+            }
+
+            //foreach (XmlNode currentNode in wholeDocument)
+            //{
+            //    if(currentNode.Name == "artist" && !artistHashSet.Contains(currentNode.InnerText))
+            //    {
+            //        artistHashSet.Add(currentNode.InnerText);
+            //        Console.WriteLine("{0} has {1} albums(s).", currentNode.InnerText, CountAuthorAlbums(currentNode.InnerText));
+            //    }
+            //}
+
+            
+        }
+
+        public static int CountAuthorAlbums(string artist)
+        {
+            int currentArtistCounter = 0;
+
+            using (XmlReader reader = XmlReader.Create("../../../catalog.xml"))
+            {
+                while (reader.Read())
+                {
+                    if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "artist"))
+                    {
+                        reader.Read();
+                        if (reader.Value == artist)
+                        {
+                            currentArtistCounter++;
+                        }
+                    }
+                }
+
+                reader.Close();
+            }
+
+            return currentArtistCounter;
+        }
+    }
+}

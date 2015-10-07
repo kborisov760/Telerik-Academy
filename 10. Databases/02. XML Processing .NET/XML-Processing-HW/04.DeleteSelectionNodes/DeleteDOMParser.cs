2612861375995ce29b5cb6def8a71c09aaccd415
Kiril Borisov <borisov.kiril760@gmail.com>
2015-10-07 12:45:59 +0300

    Delete from DOM project added

---
+++ 10. Databases/02. XML Processing .NET/XML-Processing-HW/04.DeleteSelectionNodes/DeleteDOMParser.cs
@@ -0,0 +1,27 @@
+﻿namespace XMLProcessing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml;
+
+    class DeleteDOMParser
+    {
+        static void Main()
+        {
+            XmlDocument calatogDocument = new XmlDocument();
+            calatogDocument.Load("../../../catalog.xml");
+            XmlNode mainNode = calatogDocument.DocumentElement;
+
+            foreach (XmlNode childNodes in mainNode.ChildNodes)
+            {
+                if(childNodes.Name == "album")
+                {
+                    foreach (var child in childNodes.ChildNodes)
+                    {
+                        //TODO: Implement logic for search and delete
+                    }
+                }
+            }
+        }
+    }
+}

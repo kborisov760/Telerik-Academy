489b8a5e03e51a2c215a8697c056bc2b3a3e324d
Kiril Borisov <borisov.kiril760@gmail.com>
2015-10-07 14:27:41 +0300

    Extracting songs task completed

---
+++ 10. Databases/02. XML Processing .NET/XML-Processing-HW/05.ExtractAllSongs/SongsExtraction.cs
@@ -0,0 +1,28 @@
+﻿namespace XMLProcessing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml;
+
+    class SongsExtraction
+    {
+        static void Main()
+        {
+            var listOfAllSongs = new List<string>();
+
+            using (XmlReader reader = XmlReader.Create("../../../catalog.xml"))
+            {
+                while (reader.Read())
+                {
+                    if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "title"))
+                    {
+                        reader.Read();
+                        listOfAllSongs.Add(reader.Value);
+                    }
+                }
+            }
+
+            Console.WriteLine("All songs: {0}", String.Join(", ", listOfAllSongs));
+        }
+    }
+}

abb657ecf8de10d2c39bf05cf0713cbd11b78b74
Kiril Borisov <borisov.kiril760@gmail.com>
2015-10-08 23:52:45 +0300

    Text file to XML serizlization task added

---
+++ 10. Databases/02. XML Processing .NET/XML-Processing-HW/07.CreateXMLFromTextFile/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace XMLProcessing
+{
+    using System;
+
+    public class Person
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public Person() { }
+
+        public Person(string FullName, string PhoneNumber, string Address)
+        {
+            this.FullName = FullName;
+            this.PhoneNumber = PhoneNumber;
+            this.Address = Address;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Full name: {0}\nPhone number: {1}\nAddress: {2}\n",
+                this.FullName, this.PhoneNumber, this.Address);
+        }
+    }
+}
---
+++ 10. Databases/02. XML Processing .NET/XML-Processing-HW/07.CreateXMLFromTextFile/TextFileToXml.cs
@@ -0,0 +1,61 @@
+﻿namespace XMLProcessing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    class TextFileToXml
+    {
+        static void Main()
+        {
+            var people = new List<Person>();
+            string currentLine = string.Empty;
+            int peopleCounter = 0;
+
+            using (StreamReader reader = new StreamReader("../../person.txt"))
+            {
+                while (!String.IsNullOrEmpty(currentLine = reader.ReadLine()))
+                {
+                    if (currentLine != null)
+                    {
+                        var currentPerson = new List<string>();
+                        for (int i = 0; i < 3; i++)
+                        {
+                            currentPerson.Add(currentLine);
+                            currentLine = reader.ReadLine();
+                        }
+
+                        people.Add(new Person(currentPerson[0], currentPerson[1], currentPerson[2]));
+                    }
+
+                    peopleCounter++;
+                }
+            }
+            Console.WriteLine("Task [1]");
+            Console.WriteLine("List of people: ");
+            Console.WriteLine("------------------");
+
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Task [2]");
+            Console.WriteLine("Serializing list of people...");
+
+            XmlSerializer serializedObject = new XmlSerializer(people.GetType());
+
+            using (StreamWriter outputXml = new StreamWriter("../../peopleToXml.xml"))
+            {
+                serializedObject.Serialize(outputXml, people);
+            }
+
+            Console.WriteLine("Serialization completed successfully!");
+            Console.WriteLine("----------------------------------");
+        }
+    }
+    
+    
+}

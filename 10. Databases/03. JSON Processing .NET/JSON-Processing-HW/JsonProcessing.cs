f83a80c4a9f74d4e6912554173e540252760d07c
Kiril Borisov <borisov.kiril760@gmail.com>
2015-10-09 01:14:13 +0300

    Init commit

---
+++ 10. Databases/03. JSON Processing .NET/JSON-Processing-HW/JsonProcessing.cs
@@ -0,0 +1,22 @@
+﻿namespace JsonProcessing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Web;
+    using System.Web.Script.Serialization;
+
+    class JsonProcessing
+    {
+        static void Main()
+        {
+            WebClient xmlFeed = new WebClient();
+            xmlFeed.DownloadData("https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw");
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            var xmlFeedToJson = serializer.Serialize(xmlFeed);
+
+            Console.WriteLine(xmlFeedToJson);
+        }
+    }
+}

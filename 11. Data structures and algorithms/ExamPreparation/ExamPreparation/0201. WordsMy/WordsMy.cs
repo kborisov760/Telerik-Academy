de2ba74b0ee247a082a975f33d6bc50373ba69de
Kiril Borisov <borisov.kiril76@gmail.com>
2015-12-03 01:55:18 +0200

    Logic implemented for searching words in text

--- 11. Data structures and algorithms/ExamPreparation/ExamPreparation/0201. WordsMy/WordsMy.cs
+++ 11. Data structures and algorithms/ExamPreparation/ExamPreparation/0201. WordsMy/WordsMy.cs
@@ -3,33 +3,93 @@
     using System;
     using System.Linq;
     using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using System.Text;
 
     public class WordsMy
     {
+        static Dictionary<string, int> words = new Dictionary<string, int>();
+
         public static void Main()
         {
             int T = int.Parse(Console.ReadLine());
-            var inputText = new List<string>();
-            
+            StringBuilder inputText = new StringBuilder();
+
             for (int i = 0; i < T; i++)
             {
                 var currentLine = Console.ReadLine();
-                inputText.Add(currentLine);
+                inputText.Append(currentLine);
             }
+            
+            var splittedInputText = GetWords(inputText.ToString());
+            var uniqueWords = new HashSet<string>();
 
-            var splittedInputText = new List<string[]>();
-
-            foreach (var line in inputText)
+            foreach (var word in splittedInputText)
             {
-                var currentLine = line.Split(new char[] { '/', '\\', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '-' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x).ToArray();
-                splittedInputText.Add(currentLine);
+                uniqueWords.Add(word);
             }
 
-            foreach (var item in splittedInputText)
+
+
+            foreach (var item in uniqueWords)
             {
                 Console.WriteLine(String.Join(", ", item));
-                Console.WriteLine();
             }
+        }
+
+        static bool CheckWord(string word, string original)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if(original.Contains(word[i]))
+                {
+                    counter++;
+                }
+            }
+
+            if(counter == word.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static int CountOccurences(string word, string[] input)
+        {
+            foreach (var item in input)
+            {
+                if(CheckWord(word, item) == true)
+                {
+                    //TODO: Implement logic for counting all occurences of a single word using global counter
+                }
+            }
+
+            return 0;
+        }
+
+        static string[] GetWords(string input)
+        {
+            MatchCollection matches = Regex.Matches(input, @"\b[\w']*\b");
+
+            var words = from m in matches.Cast<Match>()
+                        where !string.IsNullOrEmpty(m.Value)
+                        select TrimSuffix(m.Value);
+
+            return words.ToArray();
+        }
+
+        static string TrimSuffix(string word)
+        {
+            int apostropheLocation = word.IndexOf('\'');
+            if (apostropheLocation != -1)
+            {
+                word = word.Substring(0, apostropheLocation);
+            }
+
+            return word;
         }
     }
 }
